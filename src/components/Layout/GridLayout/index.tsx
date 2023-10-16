9e2bebf4e87b445a0d7017c2c495b158f0363def
Edwin Hernandez <Edwinhern.16@gmail.com>
2023-10-15 19:37:45 -0500

    Fix Navbar

--- src/components/Layout/GridLayout/index.tsx
+++ src/components/Layout/GridLayout/index.tsx
@@ -11,34 +11,33 @@ export const GridLayout = () => {
   return (
     <>
       <div className='space-y-4'>
+        {/* About Me and show a picture of the companies, and where I worked */}
         <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4'>
           <Card>
             <Image
-              src='https://source.unsplash.com/random/300x200?tech'
-              alt='Random tech image from Unsplash'
+              src='https://source.unsplash.com/random/1080x920?tesla'
+              alt='Companies I worked at'
               className='h-48 w-full rounded-t-lg object-cover'
-              width={100}
-              height={100}
+              width={300}
+              height={200}
             />
             <CardHeader className='p-4'>
-              <CardTitle className='text-sm font-medium'>
-                Modern Technology
-              </CardTitle>
+              <CardTitle className='text-sm font-medium'>About Me</CardTitle>
             </CardHeader>
             <CardContent className='p-4'>
               <div className='text-sm text-muted-foreground'>
-                The future is now, with cutting-edge tech at your fingertips.
+                Places that I worked with in my career.
               </div>
             </CardContent>
           </Card>
 
           <Card>
             <Image
-              src='https://source.unsplash.com/random/300x200?nature'
+              src='https://source.unsplash.com/random/1080x920?nature'
               alt='Random nature image from Unsplash'
               className='h-48 w-full rounded-t-lg object-cover'
-              width={100}
-              height={100}
+              width={300}
+              height={200}
             />
             <CardHeader className='p-4'>
               <CardTitle className='text-sm font-medium'>
@@ -54,11 +53,11 @@ export const GridLayout = () => {
 
           <Card>
             <Image
-              src='https://source.unsplash.com/random/300x200?architecture'
+              src='https://source.unsplash.com/random/1080x920?architecture'
               alt='Random architecture image from Unsplash'
               className='h-48 w-full rounded-t-lg object-cover'
-              width={100}
-              height={100}
+              width={300}
+              height={200}
             />
             <CardHeader className='p-4'>
               <CardTitle className='text-sm font-medium'>
@@ -74,11 +73,11 @@ export const GridLayout = () => {
 
           <Card>
             <Image
-              src='https://source.unsplash.com/random/300x200?lifestyle'
+              src='https://source.unsplash.com/random/1080x920?lifestyle'
               alt='Random lifestyle image from Unsplash'
               className='h-48 w-full rounded-t-lg object-cover'
-              width={100}
-              height={100}
+              width={300}
+              height={200}
             />
             <CardHeader className='p-4'>
               <CardTitle className='text-sm font-medium'>
@@ -92,8 +91,8 @@ export const GridLayout = () => {
             </CardContent>
           </Card>
         </div>
-
-        <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7'>
+        {/* Showcase my blog with a picture and a title, subtitle, and link */}
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-7'>
           <Card className='col-span-full p-4 md:col-span-4'>
             <CardHeader>
               <CardTitle className='text-lg font-bold'>Overview</CardTitle>
@@ -114,6 +113,7 @@ export const GridLayout = () => {
           </Card>
         </div>
 
+        {/* Contact and Location such as where I'm looking for jobs such as Remote and Dallas/Plano TX jobs */}
         <div className='grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-7'>
           <Card className='col-span-full p-4 md:col-span-2'>
             <CardHeader>
@@ -128,9 +128,9 @@ export const GridLayout = () => {
 
           <Card className='col-span-full p-4 md:col-span-5'>
             <Image
-              src='https://source.unsplash.com/random/500x300?landscape'
+              src='https://source.unsplash.com/random/1080x920?landscape'
               alt='Random landscape image from Unsplash'
-              className='h-64 w-full rounded-t-lg object-cover'
+              className='h-36 w-full rounded-t-lg object-cover'
               width={100}
               height={100}
             />
@@ -146,6 +146,39 @@ export const GridLayout = () => {
             </CardContent>
           </Card>
         </div>
+      </div>
+    </>
+  );
+};
+
+export const Grid2 = () => {
+  return (
+    <>
+      <div className='grid h-screen auto-rows-[192px] grid-cols-3 gap-4'>
+        <div
+          className='col-span-3 row-span-1 rounded-xl border-2 border-slate-400/10 bg-neutral-100 bg-cover bg-center p-4 dark:bg-neutral-900'
+          style={{ backgroundImage: 'url(path_to_image_for_Edwin_H)' }}
+        >
+          Edwin H
+        </div>
+        {[...Array(6)].map((_, i) => {
+          const images = [
+            'url(path_to_image_for_Music_Vibes)',
+            'url(https://source.unsplash.com/random/1080x920?lifestyle)',
+            // Add more image URLs here
+          ];
+          return (
+            <div
+              key={i}
+              className={`row-span-1 rounded-xl border-b border-slate-400/10 bg-neutral-100 p-4 dark:bg-neutral-900 ${
+                i === 2 || i === 5 ? 'col-span-2' : ''
+              } bg-cover bg-center`}
+              style={{ backgroundImage: images[i] || 'none' }}
+            >
+              {i === 0 ? 'Music Vibes' : ''}
+            </div>
+          );
+        })}
       </div>
     </>
   );
--- src/components/Navigation/Navbar/index.tsx
+++ src/components/Navigation/Navbar/index.tsx
@@ -1,24 +1,21 @@
 import Link from 'next/link';
 import ThemeToggleButton from '@/components/common/ThemeToggleButton';
-import { MaxWidthWrapper } from '@/components/common/InterfaceElements/MaxWidthWrapper';
 import React from 'react';
 import { Menu } from '../NavigationMenu';
 
 export const Navbar = () => {
   return (
-    <nav className='sticky inset-x-0 top-0 z-30 h-14 w-full border-b px-4 backdrop-blur-lg transition-all'>
-      <MaxWidthWrapper>
-        <div className='flex h-14 items-center justify-between'>
-          <Link className='text-lg font-semibold hover:underline' href='/'>
-            Edwin H
-          </Link>
+    <nav className='sticky inset-x-0 top-0 h-14 w-full border-b bg-background px-4'>
+      <div className='flex h-14 items-center justify-between'>
+        <Link className='text-lg font-semibold hover:underline' href='/'>
+          Edwin H
+        </Link>
 
-          <div className='flex items-center  space-x-4'>
-            <ThemeToggleButton />
-            <Menu />
-          </div>
+        <div className='flex items-center space-x-4'>
+          <ThemeToggleButton />
+          <Menu />
         </div>
-      </MaxWidthWrapper>
+      </div>
     </nav>
   );
 };
