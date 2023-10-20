db7963b9d51d77fe5104da9f8caf378b38e51447
Edwin Hernandez <Edwinhern.16@gmail.com>
2023-10-20 01:26:04 -0500

    Fix hydration issue

--- src/components/Blog/BlogPost/Buttons/CopyLink.tsx
+++ src/components/Blog/BlogPost/Buttons/CopyLink.tsx
@@ -7,10 +7,12 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { Button } from '../../../ui/button';
+import { useIsMounted } from '@/hooks/useIsMounted';
 
 type CopyLinkProps = { link: string };
 export const CopyLink: React.FC<CopyLinkProps> = ({ link }) => {
   const [hasCheckIcon, setHasCheckIcon] = useState(false);
+  const mounted = useIsMounted();
 
   const onCopy = () => {
     navigator.clipboard.writeText(link);
@@ -22,21 +24,25 @@ export const CopyLink: React.FC<CopyLinkProps> = ({ link }) => {
   };
 
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger>
-          <Button variant='outline' size={'icon'} onClick={onCopy}>
-            {hasCheckIcon ? (
-              <CheckIcon className='h-4 w-4' />
-            ) : (
-              <CopyIcon className='h-4 w-4' />
-            )}
-          </Button>
-        </TooltipTrigger>
-        <TooltipContent className='z-[2000] p-2 transition'>
-          Copy link
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    mounted && (
+      <>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Button variant='outline' size={'icon'} onClick={onCopy}>
+                {hasCheckIcon ? (
+                  <CheckIcon className='h-4 w-4' />
+                ) : (
+                  <CopyIcon className='h-4 w-4' />
+                )}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent className='z-[2000] p-2 transition'>
+              Copy link
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </>
+    )
   );
 };
--- src/components/Blog/BlogPost/Buttons/TwitterLink.tsx
+++ src/components/Blog/BlogPost/Buttons/TwitterLink.tsx
@@ -6,34 +6,39 @@ import {
   TooltipTrigger,
 } from '@/components/ui/tooltip';
 import { Button } from '../../../ui/button';
+import { useIsMounted } from '@/hooks/useIsMounted';
 
 export const TwitterLink = ({ title, baseUrl, slug }: any) => {
+  const mounted = useIsMounted();
+
   return (
-    <>
-      <div>
-        <TooltipProvider>
-          <Tooltip>
-            <TooltipTrigger>
-              <Button
-                variant='ghost'
-                size={'icon'}
-                className='inline-flex cursor-pointer p-2 transition'
-                onClick={() => {
-                  window.open(
-                    `https://twitter.com/intent/tweet?text=${title}&url=${baseUrl}/blog/${slug}`,
-                    '_blank',
-                  );
-                }}
-              >
-                <TwitterLogoIcon className='h-4 w-4 text-black dark:text-white' />
-              </Button>
-            </TooltipTrigger>
-            <TooltipContent className='z-[2000]'>
-              Share on Twitter
-            </TooltipContent>
-          </Tooltip>
-        </TooltipProvider>
-      </div>
-    </>
+    mounted && (
+      <>
+        <div>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger>
+                <Button
+                  variant='ghost'
+                  size={'icon'}
+                  className='inline-flex cursor-pointer p-2 transition'
+                  onClick={() => {
+                    window.open(
+                      `https://twitter.com/intent/tweet?text=${title}&url=${baseUrl}/blog/${slug}`,
+                      '_blank',
+                    );
+                  }}
+                >
+                  <TwitterLogoIcon className='h-4 w-4 text-black dark:text-white' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent className='z-[2000]'>
+                Share on Twitter
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </div>
+      </>
+    )
   );
 };
