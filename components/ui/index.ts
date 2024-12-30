aff48685caa449db2ff998687acaaf9bcb9a99c1
Edwin Hernandez <edwinhern.16@gmail.com>
2024-12-29 22:07:11 -0600

    refactor: :recycle: Add components and use tailwind v4

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+export * from "./primitive";
+export * from "./button";
+export * from "./heading";
+export * from "./link";
+export * from "./primitive";
+export * from "./primitive";
+export * from "./container";
