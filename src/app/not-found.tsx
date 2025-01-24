4564731f57d5193cd9e3ee1acf42474245ffa325
Edwin Hernandez <edwinhern.16@gmail.com>
2025-01-24 00:48:06 -0600

    feat: :sparkles: Add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,34 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { IconArrowUpRight } from "justd-icons";
+import type { Metadata } from "next";
+import Link from "next/link";
+
+export const metadata: Metadata = {
+	title: "404",
+	description: "Uh oh! This page does not exist",
+};
+
+export default function Custom404() {
+	return (
+		<div className="flex flex-col gap-2">
+			<h1 className="animate-in font-bold text-lg text-primary">404!</h1>
+			<p
+				className="max-w-md animate-in font-light leading-relaxed"
+				style={{ "--index": 1 } as React.CSSProperties}
+			>
+				Uh oh! This page does not exists, maybe you clicked an old link or misspelled. Please try again…
+			</p>
+
+			<div className="h-2" />
+			<Link
+				className={cn(buttonVariants({ variant: "outline" }), "max-w-fit animate-in")}
+				href="/"
+				style={{ "--index": 1 } as React.CSSProperties}
+			>
+				Return home
+				<IconArrowUpRight className="size-4" />
+			</Link>
+		</div>
+	);
+}
