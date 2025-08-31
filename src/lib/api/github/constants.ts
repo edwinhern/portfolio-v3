d545a3448caffd718850888b34948d0eefd9803d
Edwin Hernandez <edwinhern.16@gmail.com>
2025-08-31 22:01:18 +0000

    chore: Update .gitignore to include .pnpm-store, add lint and format scripts to package.json, and set up devcontainer configuration with Docker support

--- src/lib/api/github/constants.ts
+++ src/lib/api/github/constants.ts
@@ -1,7 +1,5 @@
 export const GITHUB_API_CONFIG = {
-	BASE_URL: "https://gh-pinned-repos-tsj7ta5xfhep.deno.dev",
 	GRAPHQL_API: "https://api.github.com/graphql",
-	USERNAME: "edwinhern",
 	TOKEN: process.env.GITHUB_API_TOKEN || "",
 	CACHE_DURATION: 3600,
 } as const;
